0b54516ade3a67f5274e18e1e58accc0bd922c71
dwaypro <dangrantway@gmail.com>
2019-04-29 20:24:05 -0700

    update to project wccli

---
+++ bin/assets/newproject/projectconfig/src/app/app.js
@@ -0,0 +1,42 @@
+class App extends HTMLElement {
+
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({
+            mode: 'open'
+        });
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <div id="header">
+                <image></image>
+                <h1>WC CLI!</h1>
+            </div>
+            <div>
+                <ul>
+                    <li><a>documentation</a></li>
+                    <li><a>news</a></li>
+                    <li><a>community</a></li>
+                </ul>
+            </div>
+            
+
+        `
+        shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+
+    attributeChangedCallback(name, oldValue, newValue) {
+
+    }
+
+    connectedCallback() {
+    }
+}
+
+customElements.define('new-app', App);
+
+export {App}
+            
+        
