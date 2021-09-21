761893f97c0e194c47f2390dc83b9f2195f98777
dwaypro <dangrantway@gmail.com>
2021-09-21 08:57:54 -0700

    updates

--- bin/assets/newproject/projectconfig/src/app/app.js
+++ bin/assets/newproject/projectconfig/src/app/app.js
@@ -1,5 +1,31 @@
 import appHTML from "./app.html";
 import appCSS from "./app.css";
+
+/*
+ *	Property of: PetroleumRx, Inc. - 424 East Southern Avenue, Suite 101 Tempe, Arizona 85282
+ *	
+ *	PROPRITARY RIGHTS NOTICE: All rights reserved. No part of this material may be reproduced
+ *	or transmitted in any form or by any means, electronic, mechanical or otherwise, including
+ *	photocopying and recording or in connection with any information storage or retrieval,
+ *	without the permission in writing.
+ *	
+ *	Copyright - @2017 All rights reserved
+ *	
+ *	Author:	
+ *	
+ *	Purpose: 
+ *
+ *  Last Changed:
+ *	
+ */
+
+ /**
+  * 
+  * @param {*}  
+  *   
+  * @returns 
+  */
+ 
 class App extends HTMLElement {
 
     constructor() {
@@ -33,7 +59,7 @@ class App extends HTMLElement {
         }
     }
 
-    appInitialized(boolean){
+    setAppInitialized(boolean){
         this.appInitialized = boolean;
     }
 }
